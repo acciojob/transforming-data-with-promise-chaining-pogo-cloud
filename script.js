3cfb05bf9920882bd9f0d400e225a76bfcf9fcae
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-12-22 12:58:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
 //your JS code here. If required.
+
+function transform(){
+const p=new Promise((res)=>{
+	let num=document.querySelector("#ip");
+	let nom=num.value;
+	setTimeout(()=>{
+		res(nom);
+	},2000)
+})
+	p.then((data)=>{
+		let ele=document.querySelector("#output");
+		ele.innerHTML=`Result:${data}`;
+		return data;
+	}).then((data)=>{
+		setTimeout(()=>{
+			let ele=document.querySelector("#output");
+		    ele.innerHTML=`Result:${data}`;
+		},2000)
+		return data;
+	}).then((data)=>{
+            let ele=document.querySelector("#output");
+		    ele.innerHTML=`Result:${2*data}`;
+            data=2*data;
+		return new Promise((res)=>{
+			setTimeout(()=>{
+				res(data);
+			},1000)
+		})
+         
+		return data;
+	}).then((data)=>{
+		 let ele=document.querySelector("#output");
+          data=data-3;
+		 ele.innerHTML=`Result:${data-3}`;
+		 return new Promise((res)=>{
+			 setTimeout(()=>{
+				 res(data); 
+			 },1000)
+		 })
+		 
+         return data;
+
+	}).then((data)=>{
+		let ele=document.querySelector("#output");
+		    ele.innerHTML=`Result:${data/2}`;
+             data=data/2;
+             return new Promise((res)=>{
+			 setTimeout(()=>{
+				 res(data);
+			 },1000)
+		 })
+         
+         return data;
+	}).then((data)=>{
+		let ele=document.querySelector("#output");
+		    ele.innerHTML=`Final Result:${data+10}`;
+            return new Promise((res)=>{
+			 setTimeout(()=>{
+				 res(data);
+			 },1000)
+		 })
+            
+	})
+}
+
+transform();
